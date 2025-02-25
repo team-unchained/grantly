f4e9e3be6647eb76d1dcf5c50d82939c0675d0f0
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-02-26 08:19:13 +0900

    feat: usecase의 구현체인 UserService 클래스 추가

---
+++ src/api/src/main/kotlin/grantly/user/application/service/UserService.kt
@@ -0,0 +1,35 @@
+package grantly.user.application.service
+
+import grantly.common.annotations.UseCase
+import grantly.user.application.port.`in`.EditProfileUseCase
+import grantly.user.application.port.`in`.FindUserQuery
+import grantly.user.application.port.`in`.SignUpUseCase
+import grantly.user.application.port.out.UserRepository
+import grantly.user.domain.User
+
+@UseCase
+class UserService(
+    private val userRepository: UserRepository
+): SignUpUseCase, FindUserQuery, EditProfileUseCase {
+
+    override fun signUp(email: String, password: String, name: String): User {
+        return userRepository.createUser(email, password, name)
+    }
+
+    override fun findUserById(id: Long): User {
+        val user = try {
+            userRepository.getUser(id)
+        } catch (e: RuntimeException) {
+            throw Exception("User not found")
+        }
+        return user
+    }
+
+    override fun findAllUsers(): List<User> {
+        return userRepository.getAllUsers()
+    }
+
+    override fun update(id: Long, name: String): User {
+        return userRepository.updateUser(id, name)
+    }
+}
