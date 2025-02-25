dbe665b90fdf06d0de0910c855642769e8a3fc42
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-02-26 08:17:16 +0900

    feat: User 도메인 모델 수정

---
+++ src/api/src/main/kotlin/grantly/user/domain/User.kt
@@ -0,0 +1,20 @@
+package grantly.user.domain
+
+import java.time.OffsetDateTime
+
+data class User(
+    val id: Long = 0,
+    val email: String,
+    val password: String,
+    var name: String,
+    var createdAt: OffsetDateTime = OffsetDateTime.now(),
+    var modifiedAt: OffsetDateTime? = null,
+
+) {
+    fun hashPassword():String {
+        // TODO: 실제 해싱 알고리즘 적용
+        val hashedPwd = "hashed password"
+        return hashedPwd
+    }
+}
+
