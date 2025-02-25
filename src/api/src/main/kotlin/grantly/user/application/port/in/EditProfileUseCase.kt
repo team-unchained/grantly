3c83e775f84314bb5cf1a230b6a6a0b0ed9cdc50
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-02-26 08:15:17 +0900

    feat: user 관련 usecase 정의

---
+++ src/api/src/main/kotlin/grantly/user/application/port/in/EditProfileUseCase.kt
@@ -0,0 +1,9 @@
+package grantly.user.application.port.`in`
+
+import grantly.common.annotations.UseCase
+import grantly.user.domain.User
+
+@UseCase
+interface EditProfileUseCase {
+    fun update(id: Long, name:String): User
+}
---
+++ src/api/src/main/kotlin/grantly/user/application/port/in/SignUpUseCase.kt
@@ -0,0 +1,9 @@
+package grantly.user.application.port.`in`
+
+import grantly.common.annotations.UseCase
+import grantly.user.domain.User
+
+@UseCase
+interface SignUpUseCase {
+    fun signUp(email: String, password: String, name: String): User
+}
