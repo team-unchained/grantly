fe6f489ebcdcdcd1e33f19e102a7c2b6c8ed3d04
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-03-08 07:51:22 +0900

    feat: 요청 유효성 검사와 관련된 예외 핸들러 추가

--- src/api/src/main/kotlin/grantly/common/exceptions/GlobalExceptionHandler.kt
+++ src/api/src/main/kotlin/grantly/common/exceptions/GlobalExceptionHandler.kt
@@ -1,8 +1,11 @@
 package grantly.common.exceptions
 
+import jakarta.validation.ConstraintViolationException
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
@@ -28,6 +31,51 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
             )
 
         return ResponseEntity(exceptionResponse, exception.errorType.httpStatus)
+    }
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? {
+        val errors = ex.bindingResult.fieldErrors.associate { it.field to it.defaultMessage.orEmpty() }
+        val httpErrorType = HttpErrorType.UNPROCESSABLE_ENTITY
+        val exceptionResponse =
+            HttpExceptionResponse(
+                httpErrorType.httpStatus.value(),
+                httpErrorType.message,
+                errors,
+            )
+        return ResponseEntity(exceptionResponse, httpErrorType.httpStatus)
+    }
+
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun handleConstraintViolationException(ex: ConstraintViolationException): ResponseEntity<Any?> {
+        val errors = ex.constraintViolations.associate { it.propertyPath.toString() to it.message }
+        val httpErrorType = HttpErrorType.UNPROCESSABLE_ENTITY
+        val exceptionResponse =
+            HttpExceptionResponse(
+                httpErrorType.httpStatus.value(),
+                httpErrorType.message,
+                errors,
+            )
+        return ResponseEntity(exceptionResponse, httpErrorType.httpStatus)
+    }
+
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? {
+        val httpErrorType = HttpErrorType.BAD_REQUEST
+        val exceptionResponse =
+            HttpExceptionResponse(
+                httpErrorType.httpStatus.value(),
+                ex.message ?: httpErrorType.message,
+            )
+        return ResponseEntity(exceptionResponse, httpErrorType.httpStatus)
     }
 
     override fun handleExceptionInternal(
