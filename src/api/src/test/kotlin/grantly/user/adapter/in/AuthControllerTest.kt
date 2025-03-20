be5afaa845636de2802e04ade684d8cc622a2c09
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-03-20 20:29:13 +0900

    test: 로그인 API 테스트 코드

--- src/api/src/test/kotlin/grantly/user/adapter/in/AuthControllerTest.kt
+++ src/api/src/test/kotlin/grantly/user/adapter/in/AuthControllerTest.kt
@@ -1,19 +1,25 @@
 package grantly.user.adapter.`in`
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import grantly.user.adapter.`in`.dto.LoginRequest
 import grantly.user.adapter.`in`.dto.SignUpRequest
 import grantly.user.application.port.out.UserRepository
 import grantly.user.domain.User
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.env.Environment
 import org.springframework.http.MediaType
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -21,6 +27,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AuthControllerTest(
     @Autowired
     private val userRepository: UserRepository,
@@ -28,14 +35,23 @@ class AuthControllerTest(
     private val mockMvc: MockMvc,
     @Autowired
     private val objectMapper: ObjectMapper,
+    @Autowired
+    private val env: Environment,
+    @Autowired
+    private val passwordEncoder: BCryptPasswordEncoder,
 ) {
+    private val sessionTokenKey: String by lazy { env.getProperty("grantly.auth.token.key", "") }
+
+    private lateinit var existingUser: User
+
+    @BeforeAll
+    fun setUp() {
+        existingUser = createTestUser("test@email.com", "test", "test123!")
+    }
+
     @Test
     @DisplayName("중복 이메일로 회원가입 불가")
     fun `should return 409 when email is already taken`() {
-        // given
-        val existingUser = User(email = "test@email.com", name = "test", password = "test123!")
-        userRepository.createUser(existingUser)
-
         // when & then
         val jsonBody = objectMapper.writeValueAsString(SignUpRequest(existingUser.email, "test2", "test123!"))
         mockMvc
@@ -78,5 +94,47 @@ class AuthControllerTest(
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(jsonBody),
             ).andExpect(status().isUnprocessableEntity)
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    fun `should return 200 when login is successful`() {
+        // given
+        val jsonBody = objectMapper.writeValueAsString(LoginRequest(existingUser.email, "test123!"))
+
+        // when & then
+        mockMvc
+            .perform(
+                post("/v1/auth/token")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonBody),
+            ).andExpect(status().isOk)
+            .andExpect(jsonPath("$.token").exists())
+            .andExpect(cookie().exists(sessionTokenKey))
+    }
+
+    @Test
+    @DisplayName("로그인 실패: 비밀번호 불일치")
+    fun `should return 401 when password is incorrect`() {
+        // given
+        val jsonBody = objectMapper.writeValueAsString(LoginRequest(existingUser.email, "wrong-password"))
+
+        // when & then
+        mockMvc
+            .perform(
+                post("/v1/auth/token")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonBody),
+            ).andExpect(status().isUnauthorized)
+    }
+
+    fun createTestUser(
+        email: String,
+        name: String,
+        password: String,
+    ): User {
+        val user = User(email = email, name = name, password = password)
+        user.hashPassword(passwordEncoder)
+        return userRepository.createUser(user)
     }
 }
