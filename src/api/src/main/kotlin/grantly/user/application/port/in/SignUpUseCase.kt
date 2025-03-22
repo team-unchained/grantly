f9babf87559967c1b40f4cb664a5e024e3cb64fd
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-03-22 23:32:21 +0900

    fix: 불필요한 annotation 제거

--- src/api/src/main/kotlin/grantly/user/application/port/in/SignUpUseCase.kt
+++ src/api/src/main/kotlin/grantly/user/application/port/in/SignUpUseCase.kt
@@ -1,10 +1,8 @@
 package grantly.user.application.port.`in`
 
-import grantly.common.annotations.UseCase
 import grantly.user.application.port.`in`.dto.SignUpParams
 import grantly.user.domain.User
 
-@UseCase
 interface SignUpUseCase {
     fun signUp(params: SignUpParams): User
 }
