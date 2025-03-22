507e08f4171132bf0a36dcdce1b2bd38a8d10edc
Carrole <eundori.dev@gmail.com>
2025-03-22 16:11:24 +0900

    feat: axiosInstance.ts 생성하여 인스턴스 분리

---
+++ src/frontend/src/api/axiosInstance.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosInstance } from 'axios';
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
