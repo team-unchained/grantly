9e1e80a36547e5ff38a379b29681ae9798eb1b6f
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-03-24 16:50:43 +0900

    feat: http cookie 관련 유틸 추가

--- src/api/src/main/kotlin/grantly/common/utils/HttpUtil.kt
+++ src/api/src/main/kotlin/grantly/common/utils/HttpUtil.kt
@@ -1,9 +1,15 @@
 package grantly.common.utils
 
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import java.net.URI
 
 class HttpUtil {
+    @Value("\${grantly.cookie.domain}")
+    private lateinit var cookieDomain: String
+
     companion object {
         fun buildLocationURI(location: String): URI {
             val re = Regex("/v[0-9]+/.*")
@@ -16,6 +22,32 @@ class HttpUtil {
                     .build()
                     .toUri()
             }
+        }
+
+        fun buildCookie(
+            key: String,
+            value: String,
+        ) = CookieBuilder(key, value)
+    }
+
+    class CookieBuilder(
+        val name: String,
+        val value: String,
+    ) {
+        private val cookie = Cookie(name, value)
+
+        fun maxAge(maxAge: Int) = apply { cookie?.maxAge ?: maxAge }
+
+        fun domain(domain: String) = apply { cookie?.domain ?: domain }
+
+        fun secure(secure: Boolean) = apply { cookie?.secure ?: secure }
+
+        fun httpOnly(httpOnly: Boolean) = apply { cookie?.isHttpOnly ?: httpOnly }
+
+        fun sameSite(sameSite: String) = apply { cookie?.setAttribute("SameSite", sameSite) }
+
+        fun build(response: HttpServletResponse) {
+            response.addCookie(cookie)
         }
     }
 }
