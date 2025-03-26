9b81bd06db9733d2e073714fb5c29fe8df023a2e
Carrole <eundori.dev@gmail.com>
2025-03-26 21:21:06 +0900

    feat: passwordInput component 추가 후 FormInputField에서 타입에 따라 분기하여 렌더하도록 함

--- src/frontend/src/components/common/FormInputField.tsx
+++ src/frontend/src/components/common/FormInputField.tsx
@@ -1,6 +1,8 @@
 import { Path, UseFormRegister } from 'react-hook-form';
 import { Input } from '@grantly/components/ui/input';
 import { Label } from '@grantly/components/ui/label';
+import { PasswordInput } from '@grantly/components/common/PasswordInput';
+import { useCallback } from 'react';
 
 interface FormFieldProps<T extends Record<string, unknown>> {
   id: Path<T>;
@@ -15,10 +17,21 @@ export const FormInputField = <T extends Record<string, unknown>>({
   type = 'text',
   error,
   register,
-}: FormFieldProps<T>) => (
-  <div className="grid gap-2">
-    <Label htmlFor={String(id)}>{label}</Label>
-    <Input id={String(id)} type={type} {...register(id)} />
-    {error && <p className="body3 text-red-500">{error}</p>}
-  </div>
-);
+}: FormFieldProps<T>) => {
+  const renderInput = useCallback(() => {
+    switch (type) {
+      case 'password':
+        return <PasswordInput id={String(id)} {...register(id)} />;
+      default:
+        return <Input id={String(id)} type={type} {...register(id)} />;
+    }
+  }, [id, register, type]);
+
+  return (
+    <div className="grid gap-2">
+      <Label htmlFor={String(id)}>{label}</Label>
+      {renderInput()}
+      {error && <p className="body3 text-red-500">{error}</p>}
+    </div>
+  );
+};
