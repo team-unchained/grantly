851d814252f95025095f192c5f0be856c7284c89
Carrole <eundori.dev@gmail.com>
2025-04-11 15:20:11 +0900

    feat: dotenv-flow 사용해 환경 변수 관리

--- src/frontend/src/api/auth/authApi.ts
+++ src/frontend/src/api/auth/authApi.ts
@@ -7,6 +7,6 @@ import { axiosInstance } from '../axiosInstance';
 export const CreateUser = async (
   params: CreateUserRequestType
 ): Promise<CreateUserResponseType> => {
-  const response = await axiosInstance.post('v1/auth/signup', params);
+  const response = await axiosInstance.post('/v1/auth/signup', params);
   return response.data;
 };
