da39f26d68519eb6c77ea97aeee9be9d5ca2025f
Carrole <eundori.dev@gmail.com>
2025-04-12 14:18:29 +0900

    dotenv-flow 사용해 환경 변수 관리 (#58)
    * feat: dotenv-flow 사용해 환경 변수 관리
    
    * feat: yarn.lock 업데이트

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -1,4 +1,9 @@
 import type { NextConfig } from 'next';
+import dotenvFlow from 'dotenv-flow';
+
+dotenvFlow.config({
+  path: './environments',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
