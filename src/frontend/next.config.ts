1178d41f8eb89993853ae979eac238e045e4d98e
w0nder <154417771+w0nder-official@users.noreply.github.com>
2025-04-16 12:20:37 +0900

    front ENV 처리 로직 개선 (#61)
    * gitignore 정리
    
    * .env 파일 커밋 제거
    
    * dotenv-flow  처리 로직 개선

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from 'next';
 import dotenvFlow from 'dotenv-flow';
 
+// dotenv-flow 라이브러리 사용
 dotenvFlow.config({
+  // 환경 변수에서 ENVIRONMENT 값을 가져와 NODE_ENV로 사용
+  // 설정되지 않은 경우 'development' 기본값 사용
+  node_env: process.env.ENVIRONMENT || 'local',
+
+  // 디버그 모드 (환경 변수 로드 과정 출력)
+  debug: process.env.DEBUG === 'true',
   path: './environments',
 });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
