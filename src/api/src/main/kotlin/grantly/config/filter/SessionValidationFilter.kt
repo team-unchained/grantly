321ca573d6dda43754a257dae8b3d03eb2db864f
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-04-17 13:30:15 +0900

    feat: SecurityConfig 에 커스텀 필터 체인 추가

--- src/api/src/main/kotlin/grantly/config/filter/SessionValidationFilter.kt
+++ src/api/src/main/kotlin/grantly/config/filter/SessionValidationFilter.kt
@@ -11,10 +11,8 @@ import grantly.user.domain.User
 import jakarta.persistence.EntityNotFoundException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
-@Component
 class SessionValidationFilter(
     private val sessionService: SessionService,
     private val userRepository: UserRepository,
