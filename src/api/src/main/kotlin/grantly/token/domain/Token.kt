7090e02764f3f6b26085e79683c40fc0758be535
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-05-07 13:32:17 +0900

    feat: token 테이블 및 jpa entity 추가

---
+++ src/api/src/main/kotlin/grantly/token/domain/Token.kt
@@ -0,0 +1,16 @@
+package grantly.token.domain
+
+import grantly.token.adapter.out.enums.TokenType
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.OffsetDateTime
+
+@Schema(description = "토큰 도메인 모델")
+class Token(
+    val id: Long = 0L,
+    val token: String,
+    val expiresAt: OffsetDateTime,
+    val type: TokenType,
+    val payload: Map<String, Any>? = null,
+    var createdAt: OffsetDateTime = OffsetDateTime.now(),
+    var modifiedAt: OffsetDateTime? = null,
+)
---
+++ src/api/src/main/resources/migrations/V4__add_token_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE token
+(
+    id          BIGINT AUTO_INCREMENT NOT NULL,
+    created_at  datetime              NOT NULL,
+    modified_at datetime              NOT NULL,
+    token       VARCHAR(50)           NOT NULL,
+    expires_at  datetime              NOT NULL,
+    type        INT                   NOT NULL,
+    payload     JSON                  NULL,
+    CONSTRAINT pk_token PRIMARY KEY (id)
+);
+
+ALTER TABLE token
+    ADD CONSTRAINT uc_token_token UNIQUE (token);
