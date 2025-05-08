3b7f4c58ab80794a3f640f206b44c2169ddae62f
w0nder <alice@w0nder.land>
2025-05-08 16:24:31 +0900

    local 에서 caddy 로드 하는거 추가

--- docs/infra/local/scripts/hosts.sh
+++ docs/infra/local/scripts/hosts.sh
@@ -1,2 +1,3 @@
 grep -q "127.0.0.1 grantly.work" /etc/hosts || echo "\n127.0.0.1 grantly.work" | sudo tee -a /etc/hosts
 grep -q "127.0.0.1 api.grantly.work" /etc/hosts || echo "\n127.0.0.1 api.grantly.work" | sudo tee -a /etc/hosts
+grep -q "127.0.0.1 grafana.grantly.work" /etc/hosts || echo "\n127.0.0.1 grafana.grantly.work" | sudo tee -a /etc/hosts
