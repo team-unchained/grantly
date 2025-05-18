b1b50154031906d023579c90cbad60cbfa9fb0ef
w0nder <alice@w0nder.land>
2025-05-18 21:38:15 +0900

    LoiginContainer 로 변경

---
+++ src/frontend/src/components/login/LoginContainer.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { LoginForm } from '@grantly/components/login/LoginForm';
+import { useRedirectUrl } from '@grantly/hooks/useRedirectUrl';
+
+export function LoginContainer() {
+  const redirectUrl = useRedirectUrl('/dashboard');
+
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <p className="heading1">Grantly</p>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm redirectUrl={redirectUrl} />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block" />
+    </div>
+  );
+}
