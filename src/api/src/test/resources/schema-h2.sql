697673c12598da44312e669b2f662e3186eb05b0
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-06-04 13:33:47 +0900

    chore: schema-h2.sql 업데이트

--- src/api/src/test/resources/schema-h2.sql
+++ src/api/src/test/resources/schema-h2.sql
@@ -48,3 +48,22 @@ CREATE TABLE IF NOT EXISTS token
 
 ALTER TABLE token
     ADD CONSTRAINT uc_token_token UNIQUE (token);
+
+CREATE TABLE IF NOT EXISTS service
+(
+    id            BIGINT AUTO_INCREMENT NOT NULL,
+    created_at    datetime              NOT NULL,
+    modified_at   datetime              NOT NULL,
+    slug          VARCHAR(50)           NOT NULL,
+    name          VARCHAR(50)           NOT NULL,
+    image_url     VARCHAR(255)          NULL,
+    `description` VARCHAR(512)          NULL,
+    owner_id      BIGINT                NOT NULL,
+    CONSTRAINT pk_service PRIMARY KEY (id)
+);
+
+ALTER TABLE service
+    ADD CONSTRAINT uc_service_slug UNIQUE (slug);
+
+ALTER TABLE service
+    ADD CONSTRAINT FK_SERVICE_ON_OWNER FOREIGN KEY (owner_id) REFERENCES member (id);
