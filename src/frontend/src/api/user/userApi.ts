4129f1d53dab26637e98c54266b02d746b313f1c
w0nder <154417771+w0nder-official@users.noreply.github.com>
2025-06-06 12:30:50 +0900

    Fix/extra (#104)
    * env.local 커밋되게 함
    
    * url users -> members  로 함

--- src/frontend/src/api/user/userApi.ts
+++ src/frontend/src/api/user/userApi.ts
@@ -2,6 +2,6 @@ import { axiosInstance } from '@grantly/api/axiosInstance';
 import { UserType } from '@grantly/api/user/user.shcema';
 
 export const getMe = async (): Promise<UserType> => {
-  const response = await axiosInstance.get('/v1/users/me');
+  const response = await axiosInstance.get('/v1/members/me');
   return response.data;
 };
