bcb42a7afbd8953b081da351ea4aa2ff544b811e
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-06-20 19:27:29 +0900

    refac: core.store 의 예외 클래스 정리

--- src/api/src/main/kotlin/grantly/common/core/store/exceptions/StoreOperationFailedException.kt
+++ src/api/src/main/kotlin/grantly/common/core/store/exceptions/StoreOperationFailedException.kt
@@ -3,7 +3,7 @@ package grantly.common.core.store.exceptions
 /**
  * store operation 이 실패했을 때 발생하는 기본 예외 클래스
  */
-class StoreOperationFailedException : RuntimeException {
+open class StoreOperationFailedException : RuntimeException {
     constructor() : super("Failed to perform store operation")
     constructor(message: String) : super(message)
     constructor(message: String, cause: Throwable) : super(message, cause)
