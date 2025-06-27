a41e9e3968c672788ee44e1da7ad77a5e49c6b7e
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-06-27 12:49:04 +0900

    fix: FileSystemStorage 내 잘못된 path resolving 수정

--- src/api/src/main/kotlin/grantly/common/core/store/FileSystemStorage.kt
+++ src/api/src/main/kotlin/grantly/common/core/store/FileSystemStorage.kt
@@ -18,9 +18,11 @@ class FileSystemStorage(
 ) : Storage {
     private fun String.ensureLeadingSlash(): String = if (this.startsWith("/")) this else "/$this"
 
+    private fun String.removeLeadingSlash(): String = if (this.startsWith("/")) this.substring(1) else this
+
     private fun resolveStoragePath(): Path = Paths.get(rootDir, storageName)
 
-    private fun resolvePath(key: String): Path = resolveStoragePath().resolve(key.ensureLeadingSlash())
+    private fun resolvePath(key: String): Path = resolveStoragePath().resolve(key.removeLeadingSlash())
 
     override suspend fun put(
         key: String,
