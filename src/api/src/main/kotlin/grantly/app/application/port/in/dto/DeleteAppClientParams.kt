5fd571d8b019d2f79c161aab2bc5af64ce0b070a
w0nder <alice@w0nder.land>
2025-07-09 11:41:56 +0900

    불필요한 valid 어노테이션 제거

--- src/api/src/main/kotlin/grantly/app/application/port/in/dto/DeleteAppClientParams.kt
+++ src/api/src/main/kotlin/grantly/app/application/port/in/dto/DeleteAppClientParams.kt
@@ -1,11 +1,6 @@
 package grantly.app.application.port.`in`.dto
 
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
-
 data class DeleteAppClientParams(
-    @field:NotNull
     val appId: Long,
-    @field:NotBlank
     val clientId: String,
 )
